5ec3e5b78da2993c11336ce9fc5f558a9f3083f4
Joseph Olukunle <josepholukunle1107@gmail.com>
2023-05-02 10:36:59 +0000

    feat: Add navigation bar

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
+import { Link } from 'react-router-dom';
+
 import './App.css';
+import Joseph from './images/joseph.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
+    <>
+      {/* The navbar */}
+      <nav className='navbar sticky-top navbar-expand-lg navbar_background'>
+        <section className='container-fluid content_margin'>
+          <Link className='navbar-brand' to='/'>
+            <img src={Joseph} alt='My profile thumbnail' width='60' height='48' />
+          </Link>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <section class="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <Link class="nav-link color_text" to='/'>About</Link>
+              </li>
+              <li class="nav-item">
+                <Link class="nav-link color_text" to='/'>Projects</Link>
+              </li>
+            </ul>
+          </section>
+        </section>
+      </nav>
+    </>
   );
 }
 
